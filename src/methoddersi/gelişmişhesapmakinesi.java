5d2dbb0c282e07742e753fca7715c91b0965b5c8
volkankgil <104678474+volkankgil@users.noreply.github.com>
2022-07-07 19:44:28 +0300

    ss

---
+++ src/methoddersi/gelişmişhesapmakinesi.java
@@ -0,0 +1,15 @@
+package methoddersi;
+
+import java.util.Scanner;
+
+public class geliþmiþhesapmakinesi {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        
+
+
+
+    }
+}
